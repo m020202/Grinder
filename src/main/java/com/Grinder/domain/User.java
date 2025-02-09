10a954c2ed8f913296d979cf79806978cd93d672
m020202 <m020202@naver.com>
2025-02-09 16:00:08 +0900

    FEAT: 테스트용 도메인 추가

---
+++ src/main/java/com/Grinder/domain/User.java
@@ -0,0 +1,23 @@
+package com.Grinder.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Entity
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
