21faed7d1d986bfd84adb82cea3a64a0f6638ad8
m020202 <m020202@naver.com>
2025-02-13 14:45:16 +0900

    REFACTOR: docker-compose 없이 사용

--- nginx/deploy.sh
+++ nginx/deploy.sh
@@ -5,19 +5,21 @@ DEFAULT_CONF=" /etc/nginx/nginx.conf"
 
 # blue가 실행 중이면 green을 up
 if [ -z $IS_GREEN_EXIST ];then
-  docker-compose pull m020202/green:latest
-  docker-compose up -d green
+  docker image rm m020202/green:latest
+  docker run -d -p 8082:8080 --name green m020202/green:latest
   sleep 2
   sudo ln -s -f /etc/nginx/sites-available/green /etc/nginx/sites-enabled/default
   sudo nginx -s reload
-  docker-compose stop blue
+  docker stop blue
+  docker rm blue
 
 # green이 실행 중이면 blue를 up
 else
-  docker-compose pull m020202/blue:latest
-  docker-compose up -d blue
+  docker image rm m020202/blue:latest
+  docker run -d -p 8081:8080 --name blue m020202/blue:latest
   sleep 2
   sudo ln -s -f /etc/nginx/sites-available/blue /etc/nginx/sites-enabled/default
-  docker-compose stop green
+  docker stop green
+  docker rm green
 fi
 
