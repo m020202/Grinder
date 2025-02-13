ceef1cfa2223c173bcc52539b3c681c8f29f42c5
m020202 <m020202@naver.com>
2025-02-13 15:15:13 +0900

    REFACTOR: sudo 제거

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,8 @@ if [ -z $IS_GREEN_EXIST ];then
   docker image rm m020202/green:latest
   docker run -d -p 8082:8080 --name green m020202/green:latest
   sleep 2
-  sudo ln -s -f /etc/nginx/sites-available/green /etc/nginx/sites-enabled/default
-  sudo nginx -s reload
+  ln -s -f /etc/nginx/sites-available/green /etc/nginx/sites-enabled/default
+  nginx -s reload
   docker stop blue
   docker rm blue
 
