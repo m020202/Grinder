4ba8a494565fb9fcbb153681baca9030f8bee173
m020202 <m020202@naver.com>
2025-02-13 14:10:24 +0900

    CI: 배포체크

--- nginx/deploy.sh
+++ nginx/deploy.sh
@@ -5,7 +5,7 @@ DEFAULT_CONF=" /etc/nginx/nginx.conf"
 
 # blue가 실행 중이면 green을 up
 if [ -z $IS_GREEN_EXIST ];then
-  docker-compose pull m020202/grinder:latest
+  docker-compose pull m020202/green:latest
   docker-compose up -d green
   sleep 3
   sudo ln -s -f /etc/nginx/sites-available/green /etc/nginx/sites-enabled/default
@@ -14,7 +14,7 @@ if [ -z $IS_GREEN_EXIST ];then
 
 # green이 실행 중이면 blue를 up
 else
-  docker-compose pull m020202/grinder:latest
+  docker-compose pull m020202/blue:latest
   docker-compose up -d blue
   sleep 3
   sudo ln -s -f /etc/nginx/sites-available/blue /etc/nginx/sites-enabled/default
