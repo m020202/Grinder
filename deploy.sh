de5cd8c860aadba544bdb0ef97db65f7263fd650
m020202 <m020202@naver.com>
2025-04-01 09:04:27 +0900

    [CI]: 배포 체크

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
-IS_GREEN_EXIST=$(docker ps | grep green)
+IS_GREEN_EXIST=$(docker ps | grep blue)
 DEFAULT_CONF=" /etc/nginx/nginx.conf"
 
 # blue가 실행 중이면 green을 up
 if [ -z $IS_GREEN_EXIST ];then
-  docker image rm m020202/green:latest
-  docker run -d -p 8082:8080 --name green m020202/green:latest
+  docker run -d -p 8081:8080 --name green m020202/grinder:latest
   sleep 2
-  sudo ln -s -f /etc/nginx/sites-available/green /etc/nginx/sites-enabled/default
+  sudo sh -c 'echo "set \$service_url green;" > /etc/nginx/conf.d/service_url.inc'
   sudo nginx -s reload
-  docker stop blue
-  docker rm blue
+  docker rm -f blue
 
 # green이 실행 중이면 blue를 up
 else
-  docker image rm m020202/blue:latest
-  docker run -d -p 8081:8080 --name blue m020202/blue:latest
+  docker run -d -p 8080:8080 --name blue m020202/grinder:latest
   sleep 2
-  sudo ln -s -f /etc/nginx/sites-available/blue /etc/nginx/sites-enabled/default
+  sudo sh -c 'echo "set \$service_url blue;" > /etc/nginx/conf.d/service_url.inc'
   sudo nginx -s reload
-  docker stop green
-  docker rm green
+  docker rm -f green
 fi
 
